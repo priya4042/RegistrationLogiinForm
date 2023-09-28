af0ee7a1c32c610ec8974681cf85e35c97488745
Priya <priya836thakur101@gmail.com>
2023-09-28 23:07:30 +0530

    First commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RegistrationLogiinForm.Data;
+using RegistrationLogiinForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationLogiinForm.Controllers
+{
+    public class AccountController : Controller
+    {
+        private DataAccess dataAccess;
+
+        public AccountController(IConfiguration configuration)
+        {
+            dataAccess = new DataAccess(configuration);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isRegistered = dataAccess.RegisterUser(user);
+                if (isRegistered)
+                    return RedirectToAction("Login");
+                else
+                    ModelState.AddModelError("", "Error in registration");
+            }
+            return View(user);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isValidUser = dataAccess.ValidateUser(user.Email, user.Password);
+                if (isValidUser)
+                {
+                    // Store user information in session
+                    HttpContext.Session.SetString("Email", user.Email);
+
+                    return RedirectToAction("Dashboard", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Invalid email or password");
+            }
+            return View(user);
+        }
+
+        public IActionResult Logout()
+        {
+            // Remove user information from session on logout
+            HttpContext.Session.Remove("Email");
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Data/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using RegistrationLogiinForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationLogiinForm.Data
+{
+    public class DataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool RegisterUser(User user)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Users (Name, Email, Password) VALUES (@Name, @Email, @Password)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Name", user.Name);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Password", user.Password); // Note: hash this in real-world apps
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                return result > 0;
+            }
+        }
+
+        public bool ValidateUser(string email, string password)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Users WHERE Email = @Email AND Password = @Password";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password); // Note: hash and compare in real-world apps
+                con.Open();
+                int userCount = (int)cmd.ExecuteScalar();
+                return userCount > 0;
+            }
+        }
+    }
+}
